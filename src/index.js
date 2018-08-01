505866a4307a66945d4c577f78d0969e9db4e740
Harvey Multani <harvey.multani@gmail.com>
2018-08-01 02:36:22 +0200

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Redirect, Route, Switch } from 'react-router';
+import { Router } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import Login from './features/Login';
+import Home from './features/Home';
+const customHistory = createBrowserHistory();
+const Root = () => {
+  return (
+    <Router history={customHistory}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/app/home" component={Home} />
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </Router>
+  )
+}
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
