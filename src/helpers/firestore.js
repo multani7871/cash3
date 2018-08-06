f947bd4967ddee32aebb51c312c066b56a12056f
Harvey Multani <harvey.multani@gmail.com>
2018-08-06 19:08:53 +0200

    Implement updateOAuthToken method and doesUserExist method

--- src/helpers/firestore.js
+++ src/helpers/firestore.js
@@ -9,9 +9,37 @@ export function createNewUser(uid, email, OAuthToken) {
   .catch(error => console.log(error));
 }
 
+export function updateOAuthToken(uid, OAuthToken) {
+  db.collection('users').doc(uid).update({
+    OAuthToken: OAuthToken
+  })
+    .then(console.log('Oauth token updated'))
+    .catch(error => console.log(error));
+}
+
+export function doesUserExist(uid) {
+  return db.collection('users').doc(uid).get()
+  .then(function(user){
+      if (user.exists) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch(error => console.log(error));
+}
+
 export function deleteUserFromDB(uid) {
   db.collection('users').doc(uid)
   .delete()
     .then(console.log('user deleted'))
     .catch(error => console.log(error));
 }
+
+// export async function getUserOAuthToken(uid) {
+//   let OAuthToken;
+//   db.collection('users').doc(uid).get()
+//   .then(doc => OAuthToken = doc.data().OAuthToken)
+//   .catch(error => console.log(error));
+//   // console.log(OAuthToken);
+// }
