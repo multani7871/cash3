b4d56a7221becee0c980b3c2442926974a6522e3
Harvey Multani <harvey.multani@gmail.com>
2018-08-06 19:09:57 +0200

    Linting changes

--- src/features/Login.js
+++ src/features/Login.js
@@ -11,38 +11,48 @@ export default class Login extends Component {
     this.state = { splashScreen: false };
     this.handleGoogleLogin = this.handleGoogleLogin.bind(this);
   }
-  handleGoogleLogin() {
-    loginWithGoogle()
-      .catch(err => {
-        localStorage.removeItem(firebaseAuthKey)
-      });
-    // this will set the splashscreen until its overridden by the real firebaseAuthKey
-    localStorage.setItem(firebaseAuthKey, '1');
-  }
+
   componentWillMount() {
     // checks if we are logged in, if we are go to the home route
     if (localStorage.getItem(appTokenKey)) {
-      this.handleOAuthToken();
+      // this.handleOAuthToken();
       this.props.history.push('/app/home');
       return;
     }
-    firebaseAuth().onAuthStateChanged(user => {
+    firebaseAuth().onAuthStateChanged((user) => {
       if (user) {
         localStorage.removeItem(firebaseAuthKey);
         localStorage.setItem(appTokenKey, user.uid);
-        this.props.history.push('/app/home')
+        this.props.history.push('/app/home');
       }
-    })
+    });
   }
+
+  handleGoogleLogin() {
+    loginWithGoogle().catch((err) => {
+      console.log(err);
+      localStorage.removeItem(firebaseAuthKey);
+    });
+    // this will set the splashscreen until its overridden by the real firebaseAuthKey
+    localStorage.setItem(firebaseAuthKey, '1');
+  }
+
   render() {
-    if (localStorage.getItem(firebaseAuthKey) === '1')
+    if (localStorage.getItem(firebaseAuthKey) === '1') {
       return <Splashscreen />;
+    }
     return <LoginPage handleGoogleLogin={this.handleGoogleLogin} />;
   }
 }
 const LoginPage = ({ handleGoogleLogin }) => (
   <div className="login-container">
-    <button onClick={handleGoogleLogin}>Login</button>
+    <button type="submit" onClick={handleGoogleLogin}>
+Login
+    </button>
   </div>
-)
-const Splashscreen = () => (<p>Please Wait Loading...</p>);
+);
+const Splashscreen = () => (
+  <p>
+Please Wait Loading...
+  </p>
+);
