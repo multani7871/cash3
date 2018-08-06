56b4abf88214c90008f53562329f04072a050aca
Harvey Multani <harvey.multani@gmail.com>
2018-08-06 23:45:58 +0200

    Formatting cleanup and adding new methods

--- src/helpers/firestore.js
+++ src/helpers/firestore.js
@@ -1,45 +1,73 @@
-import {db} from '../config/constants';
+import { db } from "./firebaseClient";
 
 export function createNewUser(uid, email, OAuthToken) {
-  db.collection('users').doc(uid).set({
-    email: email,
-    OAuthToken: OAuthToken
-  })
-  .then(console.log('user created'))
-  .catch(error => console.log(error));
-}
-
-export function updateOAuthToken(uid, OAuthToken) {
-  db.collection('users').doc(uid).update({
-    OAuthToken: OAuthToken
-  })
-    .then(console.log('Oauth token updated'))
+  db.collection("users")
+    .doc(uid)
+    .set({
+      email: email,
+      OAuthToken: OAuthToken
+    })
+    .then(console.log("user created"))
     .catch(error => console.log(error));
 }
 
-export function doesUserExist(uid) {
-  return db.collection('users').doc(uid).get()
-  .then(function(user){
-      if (user.exists) {
-        return true;
-      } else {
-        return false;
-      }
+export function updateOAuthToken(uid, OAuthToken) {
+  db.collection("users")
+    .doc(uid)
+    .update({
+      OAuthToken: OAuthToken
     })
     .catch(error => console.log(error));
 }
 
+export async function doesUserExist(uid) {
+  try {
+    let user = await db.collection("users").doc(uid).get();
+    if (user.exists) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export function deleteUserFromDB(uid) {
-  db.collection('users').doc(uid)
-  .delete()
-    .then(console.log('user deleted'))
+  db.collection("users")
+    .doc(uid)
+    .delete()
+    .then(console.log("user deleted"))
     .catch(error => console.log(error));
 }
 
-// export async function getUserOAuthToken(uid) {
-//   let OAuthToken;
-//   db.collection('users').doc(uid).get()
-//   .then(doc => OAuthToken = doc.data().OAuthToken)
-//   .catch(error => console.log(error));
-//   // console.log(OAuthToken);
-// }
+export function getUserOAuthToken(uid) {
+  return db
+    .collection("users")
+    .doc(uid)
+    .get()
+    .then(function(doc) {
+      return doc.data().OAuthToken;
+    })
+    .catch(error => console.log(error));
+}
+
+export function addNewCalendarToUser(uid, calendarID) {
+  db.collection("users")
+    .doc(uid)
+    .update({
+      calendarID: calendarID
+    })
+    .catch(error => console.log(error));
+}
+
+export function getUserCalID(uid) {
+  return db
+    .collection("users")
+    .doc(uid)
+    .get()
+    .then(function(doc) {
+      return doc.data().calendarID;
+    })
+    .catch(error => console.log(error));
+}
