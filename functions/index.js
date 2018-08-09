5e959b735ded396bab0ef31f287db789297ab1ea
Harvey Multani <harvey.multani@gmail.com>
2018-08-09 19:34:27 +0200

    delete comments and add cors

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,9 @@ const googleCalendar = require("./googleCalendar");
 const { addNewCalendarToUser, addItemsToUser } = require("./firestore");
 const plaidClient = require("./plaidClient");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-// const cors = require("cors")({
-//   origin: true
-// });
+const cors = require("cors")({
+  origin: true
+});
 
 exports.createCalendar = functions.https.onRequest(async (req, res) => {
   const createCalendar = async () => {
