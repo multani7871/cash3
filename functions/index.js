92e7593969094333f93e492c174a80227a40a794
Harvey Multani <harvey.multani@gmail.com>
2018-08-09 19:34:43 +0200

    add index

--- functions/index.js
+++ functions/index.js
@@ -10,63 +10,63 @@ const cors = require("cors")({
 
 exports.createCalendar = functions.https.onRequest(async (req, res) => {
   const createCalendar = async () => {
-  const token = req.body.OAuthToken;
-  const uid = req.body.uid;
-  try {
-    const result = await googleCalendar.createCalendar(token);
-    const calendarID = result.id;
-    const result2 = await addNewCalendarToUser(uid, calendarID);
-    res.status(200).send(result2);
-  } catch (error) {
-    console.log(error);
-  }
+    const token = req.body.OAuthToken;
+    const uid = req.body.uid;
+    try {
+      const result = await googleCalendar.createCalendar(token);
+      const calendarID = result.id;
+      const result2 = await addNewCalendarToUser(uid, calendarID);
+      res.status(200).send(result2);
+    } catch (error) {
+      console.log(error);
+    }
   };
   cors(req, res, createCalendar);
 });
 
 exports.deleteCalendar = functions.https.onRequest(async (req, res) => {
   const deleteCalendar = async () => {
-  const token = req.body.OAuthToken;
-  const calID = req.body.calID;
+    const token = req.body.OAuthToken;
+    const calID = req.body.calID;
     if (!calID) {
       res.status(200).send(`${calID} not found`);
     }
-  let result;
-  try {
-    result = await googleCalendar.deleteCalendar(token, calID);
-    res.status(200).send(`${result} and ${calID} deleted`);
-  } catch (error) {
-    console.log(error);
-  }
+    let result;
+    try {
+      result = await googleCalendar.deleteCalendar(token, calID);
+      res.status(200).send(`${result} and ${calID} deleted`);
+    } catch (error) {
+      console.log(error);
+    }
   };
   cors(req, res, deleteCalendar);
 });
 
 exports.exchangePublicToken = functions.https.onRequest(async (req, res) => {
   const exchangePublicToken = async () => {
-  const publicToken = req.body.publicToken;
-  const uid = req.body.uid;
-  const institutionName = req.body.institution.name;
-  const institutionId = req.body.institution.institution_id;
-  const webhook = req.body.webhook;
-  try {
-    const payload = await plaidClient.exchangePublicToken(publicToken);
-    const itemId = payload.item_id;
-    const accessToken = payload.access_token;
-    const requestId = payload.request_id;
-    await addItemsToUser(
-      uid,
-      itemId,
-      institutionName,
-      institutionId,
-      accessToken,
-      requestId,
-      webhook
-    );
-    res.status(200).send(institutionName);
-  } catch (error) {
-    console.log(error);
-  }
+    const publicToken = req.body.publicToken;
+    const uid = req.body.uid;
+    const institutionName = req.body.institution.name;
+    const institutionId = req.body.institution.institution_id;
+    const webhook = req.body.webhook;
+    try {
+      const payload = await plaidClient.exchangePublicToken(publicToken);
+      const itemId = payload.item_id;
+      const accessToken = payload.access_token;
+      const requestId = payload.request_id;
+      await addItemsToUser(
+        uid,
+        itemId,
+        institutionName,
+        institutionId,
+        accessToken,
+        requestId,
+        webhook
+      );
+      res.status(200).send(institutionName);
+    } catch (error) {
+      console.log(error);
+    }
   };
   cors(req, res, exchangePublicToken);
 });
