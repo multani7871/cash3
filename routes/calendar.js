123a0d75a5b74979cc10d56273849e19af15b4cf
Harvey Multani <harvey.multani@gmail.com>
2018-08-10 21:03:31 +0200

    move routs to separate files

---
+++ routes/calendar.js
@@ -0,0 +1,39 @@
+const googleCalendar = require('../clients/googleClient');
+const { addNewCalendarToUser } = require('../clients/firestore');
+
+const createCalendar = async (req, res) => {
+  if (req.body === undefined) {
+    res.status(200).send('no stuff');
+    return;
+  }
+  try {
+    const token = req.body.OAuthToken;
+    const uid = req.body.uid;
+    const result = await googleCalendar.createCalendar(token);
+    const calendarID = result.id;
+    const result2 = await addNewCalendarToUser(uid, calendarID);
+    res.status(200).send(result2);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteCalendar = async (req, res) => {
+  const token = req.body.OAuthToken;
+  const calID = req.body.calID;
+  if (!calID) {
+    res.status(200).send(`${calID} not found`);
+  }
+  let result;
+  try {
+    result = await googleCalendar.deleteCalendar(token, calID);
+    res.status(200).send(`${result} and ${calID} deleted`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  createCalendar,
+  deleteCalendar,
+};
---
+++ routes/plaid.js
@@ -0,0 +1,38 @@
+const { addItemsToUser } = require('../clients/firestore');
+const plaidClient = require('../clients/plaidClient');
+
+const exchangePublicToken = async (req, res) => {
+  const publicToken = req.body.publicToken;
+  const uid = req.body.uid;
+  const institutionName = req.body.institution.name;
+  const institutionId = req.body.institution.institution_id;
+  const webhook = req.body.webhook;
+  try {
+    const payload = await plaidClient.exchangePublicToken(publicToken);
+    const itemId = payload.item_id;
+    const accessToken = payload.access_token;
+    const requestId = payload.request_id;
+    await addItemsToUser(
+      uid,
+      itemId,
+      institutionName,
+      institutionId,
+      accessToken,
+      requestId,
+      webhook,
+    );
+    res.status(200).send(institutionName);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const plaidWebHookDev = (req, res) => {
+  const payload = req.body;
+  res.status(200).send(`webhook hit w/ ${payload}`);
+};
+
+module.exports = {
+  exchangePublicToken,
+  plaidWebHookDev,
+};
