d26c4d2803fe8b01ba4b1106630b91ccb97faef7
Harvey Multani <harvey.multani@gmail.com>
2018-08-11 00:23:38 +0200

    implement delete All items functionality

--- routes/plaid.js
+++ routes/plaid.js
@@ -1,5 +1,6 @@
 const { addItemsToUser } = require('../clients/firestore');
 const plaidClient = require('../clients/plaidClient');
+const { getAllItems, deleteItemFromDB } = require('../clients/firestore');
 
 const exchangePublicToken = async (req, res) => {
   const publicToken = req.body.publicToken;
@@ -32,7 +33,43 @@ const plaidWebHookDev = (req, res) => {
   res.status(200).send(`webhook hit w/ ${payload}`);
 };
 
+const deleteAllItems = async (req, res) => {
+  const uid = req.body.uid;
+  // const uid = '5ImydqEWAlY3fSXNbh18GxVAdxl2';
+  let allItems;
+  try {
+    allItems = await getAllItems(uid);
+  } catch (error) {
+    console.log(error);
+  }
+
+  const deleteIndividualItem = async (item) => {
+    const itemId = item.itemId;
+    try {
+      const plaidDeletionStatus = await plaidClient.deleteItem(item.accessToken);
+      if (plaidDeletionStatus.deleted) {
+        console.log(plaidDeletionStatus);
+        await deleteItemFromDB(uid, itemId);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  allItems.forEach(deleteIndividualItem);
+  const message1 = `delete items for ${JSON.stringify(allItems)}`;
+  const message2 = 'no items to delete';
+  let messageToSend;
+  if (allItems.length === 0) {
+    messageToSend = message2;
+  } else {
+    messageToSend = message1;
+  }
+
+  res.status(200).send(messageToSend);
+};
+
 module.exports = {
   exchangePublicToken,
   plaidWebHookDev,
+  deleteAllItems,
 };
