c398d2a19855ecff75a89fe323c880810afed1da
Harvey Multani <harvey.multani@gmail.com>
2018-08-13 23:12:36 +0200

    change morgan settings and add new routes

--- server.js
+++ server.js
@@ -7,12 +7,25 @@ const path = require('path');
 const morgan = require('morgan');
 const { createCalendar, deleteCalendar } = require('./routes/calendar');
 const {
-  exchangePublicToken, plaidWebHook, deleteAllItems, deleteItem,
+  exchangePublicToken,
+  plaidWebHook,
+  deleteAllItems,
+  deleteItem,
+  getAllItemsClient,
 } = require('./routes/plaid');
+const {
+  createNewUser,
+  doesUserExist,
+  updateOAuthToken,
+  getUserOAuthToken,
+  getUserCalID,
+  deleteUserFromDB,
+  // saveRefreshToken,
+} = require('./routes/user');
 
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan(':method :url :status'));
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors);
 app.use(express.json());
@@ -23,6 +36,15 @@ app.post('/api/exchangePublicToken', exchangePublicToken);
 app.post('/api/plaidWebHook', plaidWebHook);
 app.post('/api/deleteAllItems', deleteAllItems);
 app.post('/api/deleteItem', deleteItem);
+app.post('/api/createNewUser', createNewUser);
+app.post('/api/doesUserExist', doesUserExist);
+app.post('/api/getAllItemsClient', getAllItemsClient);
+app.post('/api/updateOAuthToken', updateOAuthToken);
+app.post('/api/getUserOAuthToken', getUserOAuthToken);
+app.post('/api/getUserCalID', getUserCalID);
+app.post('/api/deleteUserFromDB', deleteUserFromDB);
+// app.post('/api/saveRefreshToken', saveRefreshToken);
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/build/index.html`));
