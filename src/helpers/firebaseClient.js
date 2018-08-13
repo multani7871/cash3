f77848b085f2a030c021f6979b4875b4e3601e69
Harvey Multani <harvey.multani@gmail.com>
2018-08-13 23:10:05 +0200

    Remove firestore from frontend

--- src/helpers/firebaseClient.js
+++ src/helpers/firebaseClient.js
@@ -1,23 +1,18 @@
-const firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/firestore");
+const firebase = require('firebase/app');
+require('firebase/auth');
 
 const config = {
   apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN, 
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
   databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: process.env.REACT_APP_PROJECTID
+  projectId: process.env.REACT_APP_PROJECTID,
 };
 firebase.initializeApp(config);
 const googleProvider = new firebase.auth.GoogleAuthProvider();
-googleProvider.addScope("https://www.googleapis.com/auth/calendar");
-const firestore = firebase.firestore();
-firestore.settings({ timestampsInSnapshots: true });
+googleProvider.addScope('https://www.googleapis.com/auth/calendar');
 const firebaseAuth = firebase.auth;
-const db = firestore;
 
 module.exports = {
   googleProvider,
   firebaseAuth,
-  db
 };
