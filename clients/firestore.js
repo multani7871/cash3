2327beba965dee1efbf0a4e8f61a7d6d28fcb647
Harvey Multani <harvey.multani@gmail.com>
2018-08-13 23:44:29 +0200

    add getAllItemsClient method and activate others that were commented out

--- clients/firestore.js
+++ clients/firestore.js
@@ -1,58 +1,54 @@
 const { db } = require('./firebaseClient');
 
-// exports.createNewUser = (uid, email, OAuthToken) => {
-//   db.collection("users")
-//     .doc(uid)
-//     .set({
-//       email: email,
-//       OAuthToken: OAuthToken
-//     })
-//     .then(console.log("user created"))
-//     .catch(error => console.log(error));
-// };
+exports.createNewUser = async (uid, email, OAuthToken) => {
+  await db
+    .collection('users')
+    .doc(uid)
+    .set({
+      email,
+      OAuthToken,
+    })
+    .catch(error => console.log(error));
+};
 
-// exports.updateOAuthToken = (uid, OAuthToken) => {
-//   db.collection("users")
-//     .doc(uid)
-//     .update({
-//       OAuthToken: OAuthToken
-//     })
-//     .catch(error => console.log(error));
-// };
+exports.updateOAuthToken = async (uid, OAuthToken) => {
+  await db.collection('users')
+    .doc(uid)
+    .update({
+      OAuthToken,
+    })
+    .catch(error => console.log(error));
+};
 
-// exports.doesUserExist = uid => {
-//   return db
-//     .collection("users")
-//     .doc(uid)
-//     .get()
-//     .then(function(user) {
-//       if (user.exists) {
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     })
-//     .catch(error => console.log(error));
-// };
+exports.doesUserExist = async (uid) => {
+  let userExists = false;
+  try {
+    const user = await db
+      .collection('users')
+      .doc(uid)
+      .get();
+    if (user.exists) {
+      userExists = true;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return userExists;
+};
 
-// exports.deleteUserFromDB = uid => {
-//   db.collection("users")
-//     .doc(uid)
-//     .delete()
-//     .then(console.log("user deleted"))
-//     .catch(error => console.log(error));
-// };
+exports.deleteUserFromDB = async (uid) => {
+  db.collection('users')
+    .doc(uid)
+    .delete()
+    .catch(error => console.log(error));
+};
 
-// exports.getUserOAuthToken = uid => {
-//   return db
-//     .collection("users")
-//     .doc(uid)
-//     .get()
-//     .then(function(doc) {
-//       return doc.data().OAuthToken;
-//     })
-//     .catch(error => console.log(error));
-// };
+exports.getUserOAuthToken = async uid => db
+  .collection('users')
+  .doc(uid)
+  .get()
+  .then(doc => doc.data().OAuthToken)
+  .catch(error => console.log(error));
 
 exports.addNewCalendarToUser = (uid, calendarID) => {
   db.collection('users')
@@ -63,16 +59,12 @@ exports.addNewCalendarToUser = (uid, calendarID) => {
     .catch(error => console.log(error));
 };
 
-// exports.getUserCalID = uid => {
-//   return db
-//     .collection("users")
-//     .doc(uid)
-//     .get()
-//     .then(function(doc) {
-//       return doc.data().calendarID;
-//     })
-//     .catch(error => console.log(error));
-// };
+exports.getUserCalID = async uid => db
+  .collection('users')
+  .doc(uid)
+  .get()
+  .then(doc => doc.data().calendarID)
+  .catch(error => console.log(error));
 
 exports.addItemsToUser = async (
   uid,
@@ -116,9 +108,32 @@ exports.getAllItems = async (uid) => {
   return itemIDs;
 };
 
+exports.getAllItemsClient = async (uid) => {
+  const itemIDs = [];
+  try {
+    const querySnapshot = await db
+      .collection('users')
+      .doc(uid)
+      .collection('items')
+      .get();
+    querySnapshot.forEach((doc) => {
+      const id = doc.id;
+      const itemRaw = doc.data();
+      const itemData = {};
+      itemData.itemId = id;
+      itemData.institutionName = itemRaw.institutionName;
+      itemIDs.push(itemData);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  return itemIDs;
+};
+
 exports.deleteItemFromDB = async (uid, itemId) => {
   try {
-    await db.collection('users')
+    await db
+      .collection('users')
       .doc(uid)
       .collection('items')
       .doc(itemId)
@@ -136,3 +151,12 @@ exports.getAccessToken = async (uid, itemId) => db
   .get()
   .then(doc => doc.data().accessToken)
   .catch(error => console.log(error));
+
+// exports.saveRefreshToken = async (uid, refreshToken) => {
+//   await db.collection('users')
+//     .doc(uid)
+//     .set({
+//       refreshToken,
+//     })
+//     .catch(error => console.log(error));
+// };
