01db2d240a382be6cde978e197b97d5cc0115381
Harvey Multani <harvey.multani@gmail.com>
2018-08-13 23:44:52 +0200

    add users changes

---
+++ routes/user.js
@@ -0,0 +1,86 @@
+const {
+  createNewUser,
+  doesUserExist,
+  updateOAuthToken,
+  getUserOAuthToken,
+  getUserCalID,
+  deleteUserFromDB,
+  // saveRefreshToken,
+} = require('../clients/firestore');
+
+exports.createNewUser = async (req, res) => {
+  const uid = req.body.uid;
+  const email = req.body.email;
+  const OAuthToken = req.body.OAuthToken;
+  try {
+    await createNewUser(uid, email, OAuthToken);
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send(`${email} created`);
+};
+
+exports.doesUserExist = async (req, res) => {
+  const uid = req.body.uid;
+  let exists;
+  try {
+    exists = await doesUserExist(uid);
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send(`${exists}`);
+};
+
+exports.updateOAuthToken = async (req, res) => {
+  const uid = req.body.uid;
+  const OAuthToken = req.body.OAuthToken;
+  try {
+    await updateOAuthToken(uid, OAuthToken);
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send();
+};
+
+exports.getUserOAuthToken = async (req, res) => {
+  const uid = req.body.uid;
+  let OAuthToken;
+  try {
+    OAuthToken = await getUserOAuthToken(uid);
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send(OAuthToken);
+};
+
+exports.getUserCalID = async (req, res) => {
+  const uid = req.body.uid;
+  let calID;
+  try {
+    calID = await getUserCalID(uid);
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send(calID);
+};
+
+exports.deleteUserFromDB = async (req, res) => {
+  const uid = req.body.uid;
+  try {
+    await deleteUserFromDB(uid);
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send();
+};
+
+// exports.saveRefreshToken = async (req, res) => {
+//   const uid = req.body.uid;
+//   const refreshToken = req.body.refreshToken;
+//   try {
+//     await saveRefreshToken(uid, refreshToken);
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   res.status(200).send();
+// };
