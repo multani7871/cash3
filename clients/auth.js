961e3a1654e4f496b6f778c98253ac20c793079a
Harvey Multani <harvey.multani@gmail.com>
2018-08-17 16:23:38 -0400

    Update auth.js
    Declare and assign at same time

--- clients/auth.js
+++ clients/auth.js
@@ -1,10 +1,9 @@
 const { firebaseAuth } = require('./firebaseClient');
 
 exports.verifyIdTokenAndReturnUid = async (idToken) => {
-  let decodedToken;
   let uid;
   try {
-    decodedToken = await firebaseAuth.verifyIdToken(idToken);
+    const decodedToken = await firebaseAuth.verifyIdToken(idToken);
     uid = decodedToken.uid;
   } catch (error) {
     console.log(error);
