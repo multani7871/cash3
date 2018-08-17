241256d5b3149c564cc8c9b18c092d2abc009c77
Harvey Multani <harvey.multani@gmail.com>
2018-08-17 16:20:57 -0400

    Remove unused code

--- clients/firebaseClient.js
+++ clients/firebaseClient.js
@@ -1,6 +1,4 @@
-const firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/firestore');
+const admin = require('firebase-admin');
 
 const config = {
   apiKey: process.env.REACT_APP_APIKEY,
@@ -8,17 +6,12 @@ const config = {
   databaseURL: process.env.REACT_APP_DATABASEURL,
   projectId: process.env.REACT_APP_PROJECTID,
 };
-
-firebase.initializeApp(config);
-const googleProvider = new firebase.auth.GoogleAuthProvider();
-googleProvider.addScope('https://www.googleapis.com/auth/calendar');
-const firestore = firebase.firestore();
+admin.initializeApp(config);
+const firebaseAuth = admin.auth();
+const firestore = admin.firestore();
 firestore.settings({ timestampsInSnapshots: true });
-const firebaseAuth = firebase.auth;
 const db = firestore;
-
 module.exports = {
-  googleProvider,
   firebaseAuth,
   db,
 };
