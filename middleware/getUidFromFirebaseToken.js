ae872b82e45db63fafaaee01a0904359fb8a06ec
Harvey Multani <harvey.multani@gmail.com>
2018-08-17 16:30:21 -0400

    try/catch

--- middleware/getUidFromFirebaseToken.js
+++ middleware/getUidFromFirebaseToken.js
@@ -3,8 +3,11 @@ const { verifyIdTokenAndReturnUid } = require('../clients/auth');
 exports.getUidFromFirebaseToken = async (req, res, next) => {
   if (req.body.idToken) {
     const idToken = req.body.idToken;
-    const uid = await verifyIdTokenAndReturnUid(idToken);
-    req.body.uid = uid;
+    try {
+      req.body.uid = await verifyIdTokenAndReturnUid(idToken);
+    } catch (error) {
+      console.log(error);
+    }
     return next();
   }
   return next();
