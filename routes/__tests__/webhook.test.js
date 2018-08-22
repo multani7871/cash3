f4ca3b036ab7bdd947d0ce82f972eb511dbd1852
Harvey Multani <harvey.multani@gmail.com>
2018-08-21 20:05:04 -0400

    comment out to implement as proper test later

--- routes/__tests__/webhook.test.js
+++ routes/__tests__/webhook.test.js
@@ -1,18 +1,18 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-async function testWebhook() {
-  try {
-    const response = await axios.post('http://localhost:5000/api/plaidWebHook', {
-      webhook_type: 'TRANSACTIONS',
-      webhook_code: 'INITIAL_UPDATE',
-      // webhook_code: 'HISTORICAL_UPDATE',
-      item_id: 'zLjJ3KQBArTkmgzBWJ6aFJgJzBz6ldco5GlLg',
-      error: null,
-      new_transactions: 19,
-    });
-    console.log(JSON.stringify(response.data));
-  } catch (error) {
-    console.log(error);
-  }
-}
-testWebhook();
+// async function testWebhook() {
+//   try {
+//     const response = await axios.post('http://localhost:5000/api/plaidWebHook', {
+//       webhook_type: 'TRANSACTIONS',
+//       webhook_code: 'INITIAL_UPDATE',
+//       // webhook_code: 'HISTORICAL_UPDATE',
+//       item_id: 'zLjJ3KQBArTkmgzBWJ6aFJgJzBz6ldco5GlLg',
+//       error: null,
+//       new_transactions: 19,
+//     });
+//     console.log(JSON.stringify(response.data));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// testWebhook();
