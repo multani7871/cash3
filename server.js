b5b35f86020268fe9e2ad2812a63fef401fae4d9
Harvey Multani <harvey.multani@gmail.com>
2018-09-05 13:35:23 -0400

    linting changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require("envkey");
-const express = require("express");
+require('envkey');
+const express = require('express');
 
 const app = express();
 
-app.use(require("./server/routes"));
+app.use(require('./server/routes'));
 
 const port = process.env.PORT || 5000;
-const announce = () => console.log(`Example app listening on ${port}!`);
+const announce = () => console.log(`Cashendar listening on ${port}!`);
 app.listen(port, announce);
