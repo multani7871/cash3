3440d6b3835b0b4cc188155120bca1048192ade1
Harvey Multani <harvey.multani@gmail.com>
2018-09-05 13:36:17 -0400

    Change deleteCalendar method and remove unused code

--- server/routeHandlers/calendar.js
+++ server/routeHandlers/calendar.js
@@ -1,15 +1,11 @@
-const googleCalendar = require('../apiClients/googleClient');
+const { createCalendar, deleteAllCalendars } = require('../controllers/googleCalendar');
 const { addNewCalendarToUser } = require('../controllers/firestore');
 
 exports.createCalendar = async (req, res) => {
-  if (req.body === undefined) {
-    res.status(200).send('no stuff');
-    return;
-  }
+  const oauthToken = req.body.OAuthToken;
+  const uid = req.body.uid;
   try {
-    const token = req.body.OAuthToken;
-    const uid = req.body.uid;
-    const result = await googleCalendar.createCalendar(token);
+    const result = await createCalendar(oauthToken);
     const calendarID = result.id;
     const result2 = await addNewCalendarToUser(uid, calendarID);
     res.status(200).send(result2);
@@ -19,14 +15,14 @@ exports.createCalendar = async (req, res) => {
 };
 
 exports.deleteCalendar = async (req, res) => {
-  const token = req.body.OAuthToken;
+  const oauthToken = req.body.OAuthToken;
   const calID = req.body.calID;
   if (!calID) {
     res.status(200).send(`${calID} not found`);
   }
   let result;
   try {
-    result = await googleCalendar.deleteCalendar(token, calID);
+    result = await deleteAllCalendars(oauthToken, calID);
     res.status(200).send(`${result} and ${calID} deleted`);
   } catch (error) {
     console.log(error);
