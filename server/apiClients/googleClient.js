42654d0c2d0277da5716476d7d5dcfea62cd547f
Harvey Multani <harvey.multani@gmail.com>
2018-11-02 03:01:24 -0400

    Eliminate need to export oauth2Client

--- server/apiClients/googleClient.js
+++ server/apiClients/googleClient.js
@@ -1,17 +1,15 @@
-const { google } = require('googleapis');
-
+const { google } = require("googleapis");
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  process.env.REDIRECT_URIS,
+  process.env.REDIRECT_URIS
 );
 
-const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
-
-module.exports = {
-  oauth2Client,
-  calendar,
+const calendar = oauthToken => {
+  oauth2Client.credentials = { access_token: oauthToken };
+  return google.calendar({ version: "v3", auth: oauth2Client });
 };
 
-// todo: find way to eliminate need to export oauth2Client
-// ideally controllers would not be aware of the oAuth2 client, might be able to do a wrapper function of some kind
+module.exports = {
+  calendar
+};
