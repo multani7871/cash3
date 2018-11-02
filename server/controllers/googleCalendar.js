2ea4d3b487eede774dcd1e7519db93e4d3537e23
Harvey Multani <harvey.multani@gmail.com>
2018-11-02 03:01:46 -0400

    Update all methods to use calendar without oauth2client

--- server/controllers/googleCalendar.js
+++ server/controllers/googleCalendar.js
@@ -1,11 +1,12 @@
-const { promisify } = require('util');
-const { oauth2Client, calendar } = require('../apiClients/googleClient');
+const { promisify } = require("util");
+const { calendar } = require("../apiClients/googleClient");
 
-exports.createCalendar = async (oauthToken) => {
-  oauth2Client.credentials = { access_token: oauthToken };
-  const insertAsync = promisify(calendar.calendars.insert).bind(calendar);
+exports.createCalendar = async oauthToken => {
+  const insertAsync = promisify(calendar(oauthToken).calendars.insert).bind(
+    calendar(oauthToken)
+  );
   const calConfig = {
-    resource: { summary: 'Cashendar' },
+    resource: { summary: "Cashendar" }
   };
   let calData;
   try {
@@ -17,14 +18,15 @@ exports.createCalendar = async (oauthToken) => {
   return calData;
 };
 
-const getAllCashendarCalendars = async (oauthToken) => {
-  oauth2Client.credentials = { access_token: oauthToken };
-  const getAllCalendarsList = promisify(calendar.calendarList.list).bind(calendar);
+const getAllCashendarCalendars = async oauthToken => {
+  const getAllCalendarsList = promisify(
+    calendar(oauthToken).calendarList.list
+  ).bind(calendar(oauthToken));
   let cashendarCalendars;
   try {
     let allCalendars = await getAllCalendarsList();
     allCalendars = allCalendars.data.items;
-    const isCalendarNameCashendar = cal => cal.summary === 'Cashendar';
+    const isCalendarNameCashendar = cal => cal.summary === "Cashendar";
     cashendarCalendars = allCalendars.filter(isCalendarNameCashendar);
   } catch (error) {
     console.log(error);
@@ -33,10 +35,11 @@ const getAllCashendarCalendars = async (oauthToken) => {
 };
 
 const deleteCalendar = async (oauthToken, calID) => {
-  oauth2Client.credentials = { access_token: oauthToken };
-  const deleteAsync = promisify(calendar.calendars.delete).bind(calendar);
+  const deleteAsync = promisify(calendar(oauthToken).calendars.delete).bind(
+    calendar(oauthToken)
+  );
   const calConfig = {
-    calendarId: calID,
+    calendarId: calID
   };
   try {
     await deleteAsync(calConfig);
@@ -45,7 +48,7 @@ const deleteCalendar = async (oauthToken, calID) => {
   }
 };
 
-const deleteOrphanCalendars = async (oauthToken) => {
+const deleteOrphanCalendars = async oauthToken => {
   let orphanCalendars;
   try {
     orphanCalendars = await getAllCashendarCalendars(oauthToken);
@@ -54,7 +57,7 @@ const deleteOrphanCalendars = async (oauthToken) => {
   }
   if (orphanCalendars.length > 0) {
     try {
-      const asyncCalendarDeletionRequests = orphanCalendars.map(async (cal) => {
+      const asyncCalendarDeletionRequests = orphanCalendars.map(async cal => {
         try {
           await deleteCalendar(oauthToken, cal.id);
         } catch (error) {
@@ -69,12 +72,44 @@ const deleteOrphanCalendars = async (oauthToken) => {
 };
 
 exports.deleteAllCalendars = async (oauthToken, calID) => {
-  oauth2Client.credentials = { access_token: oauthToken };
   try {
     await deleteCalendar(oauthToken, calID);
+    //is deleteCalendar redundant here?
     await deleteOrphanCalendars(oauthToken);
   } catch (error) {
     console.log(error);
   }
   return `${calID} deleted`;
 };
+
+// const createAllDayEvent = async (oauthToken, calID) => {
+//   const allDayEventInsertAsync = promisify(
+//     calendar(oauthToken).events.insert
+//   ).bind(calendar(oauthToken));
+
+//   const event = {
+//     summary: `test`,
+//     description: `desc`,
+//     colorId: "4",
+//     start: {
+//       dateTime: "2018-09-17T18:25:00.000-00:00",
+//       timeZone: "America/Los_Angeles"
+//     },
+//     end: {
+//       dateTime: "2018-09-18T18:25:00.000-00:00",
+//       timeZone: "America/Los_Angeles"
+//     }
+//   };
+
+//   const config = {
+//     calendarId: calID,
+//     resource: event
+//   };
+
+//   try {
+//     const result = await allDayEventInsertAsync(config);
+//     console.log(result.data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
